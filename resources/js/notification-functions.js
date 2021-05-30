b561ca72cc578693fe952f9b6b2035ac78a87c19
Rishabh Jain <rishabhjain@pop-os.localdomain>
2021-05-30 23:48:58 +0530

    Add changes to user node in firebase

--- resources/js/notification-functions.js
+++ resources/js/notification-functions.js
@@ -34,7 +34,7 @@ const displayNotificationForm = () => {
 }
 
 
-const addUserToFirebase = (user) => {
+const addUserToFirebase = (user, regionToDistrictMap) => {
     let userInfo = {
         username: user.name,
         email: user.email,
@@ -45,17 +45,25 @@ const addUserToFirebase = (user) => {
         [user.region]: "true"
     }
 
-
+    const districts = regionToDistrictMap[user.region]
     const userRef = firebase.database().ref('users/' + user.phoneNumber)
     userRef.on('value', (snapshot) => {
         const data = snapshot.val()
         if (data) {
             let updates = {}
-            updates['users/' + user.phoneNumber + '/regions/' + user.region] = region
+            for (districtId in districts) {
+                updates['users/' + user.phoneNumber + '/regions/' + districts[districtId].toString()] = region
+            }
+
             firebase.database().ref().update(updates)
         } else {
             firebase.database().ref('users/' + user.phoneNumber).set(userInfo)
-            firebase.database().ref('users/' + user.phoneNumber + '/regions').set(region)
+            let updates = {}
+            for (districtId in districts) {
+                updates['users/' + user.phoneNumber + '/regions/' + districts[districtId].toString()] = region
+            }
+
+            firebase.database().ref().update(updates)
         }
 
     })
@@ -66,10 +74,7 @@ const updateRegionInFirebase = (user, regionToDistrictMap) => {
     if (user.region) {
         const updates = {}
         const districts = regionToDistrictMap[user.region]
-        console.log(districts)
         for (districtId in districts) {
-            console.log(districtId)
-            console.log(districts[districtId])
             updates['regions/' + districts[districtId].toString() + '/users/' + user.phoneNumber] = {
                 [user.phoneNumber]: "true"
             }
